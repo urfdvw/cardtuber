09b8036eab3966900021355758997b088516815c
River Wang <urfdvw@gmail.com>
2024-05-29 22:01:47 -0400

    bounce and get volume in the same time

--- src/test_bounce_and_mic.py
+++ src/test_bounce_and_mic.py
@@ -1,10 +1,56 @@
-from time import monotonic
 import board
+import displayio
+import framebufferio
+import sharpdisplay
+import adafruit_imageload
+from adafruit_display_shapes.rect import Rect
+import gc
+from math import sin
+from time import monotonic as t
+from time import sleep
 
 from cardtuber import MicVolume
 
+jump_scale = 5
+
+displayio.release_displays()
+
+bus = board.SPI()
+chip_select_pin = board.RX
+framebuffer = sharpdisplay.SharpMemoryFramebuffer(bus, chip_select_pin, width=144, height=168, baudrate=8000000)
+
+display = framebufferio.FramebufferDisplay(framebuffer, rotation = 0)
+bitmap, palette = adafruit_imageload.load(
+    "/avatar/mceo.bmp",
+    bitmap=displayio.Bitmap,
+    palette=displayio.Palette
+)
+
+# Create a TileGrid to hold the bitmap
+tile_grid = displayio.TileGrid(bitmap, pixel_shader=palette)
+
+# Create a rectangle
+rect = Rect(0, 168 - jump_scale, 144, jump_scale, fill=0xffffff)
+
+# Create a Group to hold the TileGrid
+group = displayio.Group()
+
+# Add the TileGrid to the Group
+group.append(rect)
+group.append(tile_grid)
+
+# Add the Group to the Display
+display.root_group = group
+
+print(gc.mem_free())
+
+#%% main
+# Loop forever so you can enjoy your image
 
 mv = MicVolume(N=10, length=320)
-for i in range(1000):
+
+while True:
+    y = - abs(int(sin(t()*3)*jump_scale))
+    tile_grid.y = y
     mv.record()
     print(mv.getVolume())
